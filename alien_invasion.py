dda37fed84bc7c7bff44d8c7434bffd0952c157d
Ruhi1010 <rashedial52@gmail.com>
2025-07-11 23:38:42 +0600

    .

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,24 +2,84 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+
+class Ship:
+    def __init__(self, screen):
+        ship_image = pygame.image.load('./images/ship_1.png')
+        #self.ship_image = pygame.transform.scale(self.ship_image, (40, 50))
+        self.ship_image = pygame.transform.scale_by(ship_image, 0.08)
+        self.screen = screen
+    def blitme(self):
+        self.screen.blit(self.ship_image, (570, 670))
+
+
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, 
+            self.settings.screen_height
+        ))
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230, 230, 230)
         
-    def run_game(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        self.ship = Ship(self.screen)
+        
+        
+        self.clock = pygame.time.Clock()
+        
+        
+        self.font = pygame.font.SysFont("Arial", 24)
+
+        
+    # def run_game(self):
+    #     while True:
+    #         for event in pygame.event.get():
+    #             if event.type == pygame.QUIT:
+    #                 sys.exit()
             
-            self.screen.fill(self.bg_color)
+    #         self.screen.fill(self.settings.bg_color)
                 
-            pygame.display.flip()
+    #         pygame.display.flip()
     
-if __name__ == "__main__":
-    ai = AlienInvasion()
-    ai.run_game()
+    
+    def run_game(self):
+        x = 50
+        y = 50
+        while True:
+            events = pygame.event.get()
+            print(events)
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+            
+            self.screen.fill(self.settings.bg_color)
+            self.render_fps(self.screen, self.clock, self.font)
+            self.ship.blitme()
+            
+            pygame.draw.rect(self.screen, (0, 255, 0), (x, y, 100, 100))
+            x+=.1
+            y+=.1
+            
+            pygame.display.flip()
+            
+            self.clock.tick()
+            
+            
+    def render_fps(self, screen, clock, font):
+        fps = int(clock.get_fps())
+        fps_text = font.render(f"FPS: {fps}", True, (255, 100, 100))
+        screen.blit(fps_text, (10, 10))
+    
+# if __name__ == "__main__":
+#     ai = AlienInvasion()
+#     ai.run_game()
+
+
+
+main = AlienInvasion()
+main.run_game()
